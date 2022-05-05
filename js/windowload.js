7a58e049884e750a1e86174caa7d73e4b7ebcf3f
Manpreet Dhaliwal <msdhaliwal@outlook.in>
2022-05-05 17:12:18 +0530

    initial commit

---
+++ js/windowload.js
@@ -0,0 +1,10 @@
+async function preload() {
+	setTheme();
+	let res = await directory.findall();
+	if (res.code == dbcodes.success) {
+		for (const doc of res.docs) songPath.push(doc.path);
+		let songArr = await extractMusicFiles(songPath);
+		parseFiles(songArr);
+	}
+}
+preload();
